4edaee93de8c4f2489aab10046aea3a47eee0f71
fachriza <fachriza.hidaya98@gmail.com>
2024-11-06 13:47:36 +0700

    fix: comment

--- src/components/Tribe/Feed/PostComment/PostCommentList.js
+++ src/components/Tribe/Feed/PostComment/PostCommentList.js
@@ -18,45 +18,45 @@ const PostCommentList = ({
   navigation,
   handleRefreshComments,
 }) => {
-  const screenHeight = Dimensions.get("screen");
+  const screenHeight = Dimensions.get("screen").height;
 
   return (
-    <GestureHandlerRootView>
-      <View style={{ height: screenHeight.height - 550 }}>
-        {comments.length > 0 ? (
-          <FlashList
-            data={comments}
-            keyExtractor={(item, index) => item.id}
-            onEndReachedThreshold={0.1}
-            onScrollBeginDrag={() => setHasBeenScrolled(true)}
-            ListFooterComponent={() => hasBeenScrolled && commentIsLoading && <ActivityIndicator />}
-            onEndReached={hasBeenScrolled ? handleWhenScrollReachedEnd : null}
-            estimatedItemSize={80}
-            refreshControl={<RefreshControl refreshing={commentIsFetching} onRefresh={handleRefreshComments} />}
-            renderItem={({ item, index }) => (
-              <PostCommentListItem
-                key={index}
-                postId={item?.id}
-                parentId={item.parent_id ? item?.parent_id : item?.id}
-                authorImage={item?.employee_image}
-                authorName={item?.employee_name}
-                totalReplies={item?.total_replies}
-                comments={item?.comments}
-                handleReply={replyHandler}
-                handlePressLink={pressLinkHandler}
-                employeeUsername={employeeUsername}
-                setCommentParentId={setCommentParentId}
-                navigation={navigation}
-              />
-            )}
-          />
-        ) : (
-          <View style={{ marginTop: 20, alignItems: "center" }}>
-            <Text style={{ fontSize: 16, fontWeight: "500" }}>No Comments Yet</Text>
-          </View>
-        )}
-      </View>
-    </GestureHandlerRootView>
+    // <GestureHandlerRootView>
+    <View style={{ height: screenHeight - 550 }}>
+      {comments.length > 0 ? (
+        <FlashList
+          data={comments}
+          keyExtractor={(item, index) => item.id}
+          onEndReachedThreshold={0.1}
+          onScrollBeginDrag={() => setHasBeenScrolled(true)}
+          ListFooterComponent={() => hasBeenScrolled && commentIsLoading && <ActivityIndicator />}
+          onEndReached={hasBeenScrolled ? handleWhenScrollReachedEnd : null}
+          estimatedItemSize={80}
+          refreshControl={<RefreshControl refreshing={commentIsFetching} onRefresh={handleRefreshComments} />}
+          renderItem={({ item, index }) => (
+            <PostCommentListItem
+              key={index}
+              postId={item?.id}
+              parentId={item.parent_id ? item?.parent_id : item?.id}
+              authorImage={item?.employee_image}
+              authorName={item?.employee_name}
+              totalReplies={item?.total_replies}
+              comments={item?.comments}
+              handleReply={replyHandler}
+              handlePressLink={pressLinkHandler}
+              employeeUsername={employeeUsername}
+              setCommentParentId={setCommentParentId}
+              navigation={navigation}
+            />
+          )}
+        />
+      ) : (
+        <View style={{ marginTop: 20, alignItems: "center" }}>
+          <Text style={{ fontSize: 16, fontWeight: "500" }}>No Comments Yet</Text>
+        </View>
+      )}
+    </View>
+    // </GestureHandlerRootView>
   );
 };
 
