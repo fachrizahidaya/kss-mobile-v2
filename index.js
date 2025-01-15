66477d83b2c50d11a750a6fc8286134b9cca919c
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2025-01-15 16:52:19 +0700

    chore: - remove unnecessary - update app version

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import { AppRegistry } from "react-native/types";
 import App from "./App";
 import { name as appname } from "./app.json";
-import messaging from "@react-native-firebase/messaging";
 
-messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-  console.log("Message handled in the background!", remoteMessage);
-});
 AppRegistry.registerComponent(appname, () => App);
