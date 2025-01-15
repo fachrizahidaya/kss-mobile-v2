b4e961177972a010ff12b3a009724d9fdaf34090
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2025-01-15 14:56:35 +0700

    fix: add setbackgroundmessagehandler

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 import { AppRegistry } from "react-native/types";
 import App from "./App";
 import { name as appname } from "./app.json";
-import messaging, { getMessaging } from "@react-native-firebase/messaging";
+import messaging from "@react-native-firebase/messaging";
 
-// messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-//   console.log("Message handled in the background!", remoteMessage);
-// });
-setBackgroundMessageHandler(async (remoteMessage) => {
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
   console.log("Message handled in the background!", remoteMessage);
-});
-onMessage(getMessaging(), async (remoteMessage) => {
-  console.log("Message received in foreground:", remoteMessage);
 });
 AppRegistry.registerComponent(appname, () => App);
