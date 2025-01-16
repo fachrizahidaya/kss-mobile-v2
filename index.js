265bf15013e770e8f32586a4c76a6bffbfd45232
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2025-01-16 08:55:40 +0700

    chore: setBackgroundMessage

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { AppRegistry } from "react-native/types";
 import App from "./App";
 import { name as appname } from "./app.json";
+import messaging from "@react-native-firebase/messaging";
 
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  console.log("Message handled in the background!", remoteMessage);
+});
 AppRegistry.registerComponent(appname, () => App);
