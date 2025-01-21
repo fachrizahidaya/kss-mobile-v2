10940f7224c1131d5ed5c5a31288ce254967e5fd
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2025-01-21 11:22:39 +0700

    fix: initiation login after install

--- src/components/Tribe/Clock/ClockAttendance.js
+++ src/components/Tribe/Clock/ClockAttendance.js
@@ -109,14 +109,14 @@ const ClockAttendance = ({
   const rContainerStyle = useAnimatedStyle(() => {
     let backgroundColor;
     if (!location) {
-      backgroundColor = Colors.danger; // Red when location is null
+      backgroundColor = "#FF7F7F"; // Red when location is null
     } else if (clockIn && !minimumDurationReached) {
-      backgroundColor = Colors.danger;
+      backgroundColor = "#FF7F7F";
     } else {
       backgroundColor = interpolateColor(
         limitedTranslateX.value,
         [0, screenWidth.width - MIN_TRANSLATE_X],
-        ["#87878721", Colors.primary, Colors.danger],
+        ["#87878721", Colors.primary, "#FF7F7F"],
       );
     }
 
@@ -134,7 +134,7 @@ const ClockAttendance = ({
     if (!location) {
       backgroundColor = Colors.danger;
     } else if (clockIn && !minimumDurationReached) {
-      backgroundColor = Colors.primary;
+      backgroundColor = Colors.danger;
     } else {
       backgroundColor = interpolateColor(
         limitedTranslateX.value,
@@ -311,7 +311,7 @@ const ClockAttendance = ({
               {
                 backgroundColor:
                   location === null
-                    ? Colors.danger
+                    ? "#FF7F7F"
                     : modalIsOpen
                       ? Colors.primary
                       : "#87878721",
