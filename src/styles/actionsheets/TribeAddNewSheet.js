22c1806e5b4a0c2577133dc74fefd99f1f9b04cd
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2025-02-25 12:13:16 +0700

    fix: reminder clock in

--- src/styles/actionsheets/TribeAddNewSheet.js
+++ src/styles/actionsheets/TribeAddNewSheet.js
@@ -101,8 +101,10 @@ const TribeAddNewSheet = (props) => {
   const { data: attendance, refetch: refetchAttendance } = useFetch(
     "/hr/timesheets/personal/attendance-today"
   );
+
   const { data: profile } = useFetch("/hr/my-profile");
   const { data: myTimeGroup } = useFetch("/hr/my-time-group");
+
   const { isOpen: clockModalIsOpen, toggle: toggleClockModal } =
     useDisclosure(false);
   const { isOpen: alertIsOpen, toggle: toggleAlert } = useDisclosure(false);
@@ -455,7 +457,7 @@ const TribeAddNewSheet = (props) => {
 
   const setUserClock = async () => {
     try {
-      await insertAttend(attendance?.data?.time_in || null);
+      await insertAttend(attendance?.data?.on_duty || null);
       if (attendance?.data) {
         await insertGoHome(attendance?.data?.time_out || null);
       } else {
@@ -482,6 +484,7 @@ const TribeAddNewSheet = (props) => {
   const getUserClock = async () => {
     const storedEmployeeClockIn = await fetchAttend();
     const storedEmployeeClockOut = await fetchGoHome();
+    console.log("s", storedEmployeeClockIn);
 
     const clock_in = storedEmployeeClockIn;
     const clock_out = attendance?.data?.time_out || result?.data?.time_out;
