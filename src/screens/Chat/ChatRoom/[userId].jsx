ce0b6bc022f64e677ce85dc60983a71e5c12d54c
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2025-03-14 09:40:15 +0700

    fix: send message

--- src/screens/Chat/ChatRoom/[userId].jsx
+++ src/screens/Chat/ChatRoom/[userId].jsx
@@ -69,6 +69,11 @@ const ChatRoom = () => {
   const [requestType, setRequestType] = useState("");
   const [errorMessage, setErrorMessage] = useState(null);
   const [optimisticChat, setOptimisticChat] = useState(null);
+<<<<<<< HEAD
+=======
+
+  console.log("f", fileAttachment);
+>>>>>>> 1d2a6e87 (fix: send message)
 
   window.Pusher = Pusher;
   const { laravelEcho, setLaravelEcho } = useWebsocketContext();
@@ -523,6 +528,7 @@ const ChatRoom = () => {
         if (currentUser === null) {
           setCurrentUser(res.data?.data?.chat_personal_id);
         }
+        setOptimisticChat(null);
       },
       onSettled: () => {
         stopLoadingChat();
@@ -543,17 +549,17 @@ const ChatRoom = () => {
   const renderChats = chatIsLoading
     ? [
         {
-          message: variables?._parts[3][1],
+          message: optimisticChat?._parts[3][1],
           from_user_id: userSelector?.id,
-          file_name: variables?._parts[4][1]?.name,
-          file_path: variables?._parts[4][1]?.uri,
-          mime_type: variables?._parts[4][1]?.type,
-          project_id: variables?._parts[5][1],
-          project_no: variables?._parts[6][1],
-          project_title: variables?._parts[7][1],
-          task_id: variables?._parts[8][1],
-          task_no: variables?._parts[9][1],
-          task_title: variables?._parts[10][1],
+          file_name: optimisticChat?._parts[4][1]?.name,
+          file_path: optimisticChat?._parts[4][1]?.uri,
+          mime_type: optimisticChat?._parts[4][1]?.type,
+          project_id: optimisticChat?._parts[5][1],
+          project_no: optimisticChat?._parts[6][1],
+          project_title: optimisticChat?._parts[7][1],
+          task_id: optimisticChat?._parts[8][1],
+          task_no: optimisticChat?._parts[9][1],
+          task_title: optimisticChat?._parts[10][1],
           isOptimistic: true,
         },
         ...chatList,
