d481ac2d96bbc72c608da8fe96b322ca6a881f50
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2025-03-19 14:39:59 +0700

    feat: selfie location

--- src/components/Tribe/Clock/MapLocation.js
+++ src/components/Tribe/Clock/MapLocation.js
@@ -117,6 +117,7 @@ const styles = StyleSheet.create({
   map: {
     width: "100%",
     height: 200,
+    borderRadius: 6,
   },
 });
 >>>>>>> ed94efae (fix: map location for ios)
--- src/components/Tribe/Clock/SelfieLocation.js
+++ src/components/Tribe/Clock/SelfieLocation.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { useRef } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { Selfie } from "simple-selfie";
@@ -71,10 +72,70 @@ const styles = StyleSheet.create({
 =======
 import React from "react";
 import { Text } from "react-native";
+=======
+import { useRef } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Selfie } from "simple-selfie";
+>>>>>>> 57035468 (feat: selfie location)
 
-const SelfieLocation = () => {
-  return <Text>SelfieLocation</Text>;
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+import Button from "../../../styles/forms/Button";
+import ImageAttachment from "../../Chat/Attachment/ImageAttachment";
+import EmptyPlaceholder from "../../../layouts/EmptyPlaceholder";
+import FloatingButton from "../../../styles/buttons/FloatingButton";
+import { Colors } from "../../../styles/Color";
+
+const SelfieLocation = ({ attachment, toggle, handleAttachment }) => {
+  return (
+    <>
+      <Pressable style={styles.container} onPress={attachment ? null : toggle}>
+        {attachment ? (
+          <ImageAttachment image={attachment} setImage={handleAttachment} />
+        ) : (
+          <EmptyPlaceholder text="Select an image" />
+        )}
+        {attachment && (
+          <Pressable style={styles.camera}>
+            <MaterialCommunityIcons
+              name="camera-outline"
+              color={Colors.secondary}
+              size={20}
+              onPress={toggle}
+            />
+          </Pressable>
+        )}
+      </Pressable>
+    </>
+  );
 };
 
 export default SelfieLocation;
+<<<<<<< HEAD
 >>>>>>> ed94efae (fix: map location for ios)
+=======
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 16,
+    marginVertical: 14,
+    height: 300,
+    backgroundColor: Colors.iconGrey,
+    borderRadius: 6,
+  },
+  camera: {
+    backgroundColor: Colors.primary,
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    width: 50,
+    height: 50,
+    bottom: 20,
+    right: 10,
+    zIndex: 2,
+    borderRadius: 30,
+    borderWidth: 3,
+    borderColor: Colors.borderWhite,
+  },
+});
+>>>>>>> 57035468 (feat: selfie location)
