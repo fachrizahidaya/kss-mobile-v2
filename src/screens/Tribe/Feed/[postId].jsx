b496f4cc7273f9620ce6c46dd73d04fac9331e0a
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2025-06-04 18:29:29 +0700

    fix: post

--- src/screens/Tribe/Feed/[postId].jsx
+++ src/screens/Tribe/Feed/[postId].jsx
@@ -95,10 +95,14 @@ const Post = () => {
    * Handle add comment
    */
 <<<<<<< HEAD
+<<<<<<< HEAD
   const handleAddComment = (id) => {
     const referenceIndex = posts.findIndex((post) => post.id === id);
 =======
   const addCommentHandler = () => {
+=======
+  const handleAddComment = () => {
+>>>>>>> 7406ab7d (fix: post)
     const referenceIndex = posts.findIndex((post) => post.id === postData?.data?.id);
 >>>>>>> d6f5cf86 (fix:)
     posts[referenceIndex]["comments_count"] += 1;
@@ -118,7 +122,7 @@ const Post = () => {
 =======
       refetchPostData();
       refetchCommentHandler(setCurrentOffsetComments, setReloadComment, reloadComment);
-      addCommentHandler(postData?.data?.id);
+      handleAddComment(postData?.data?.id);
       setCommentParentId(null);
 >>>>>>> d6f5cf86 (fix:)
       setSubmitting(false);
