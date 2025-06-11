5016891f078a8980a526604d5d05c0de609a0423
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2025-06-11 11:49:52 +0700

    chore: remove unnecessary

--- src/screens/Tribe/Feed/[postId].jsx
+++ src/screens/Tribe/Feed/[postId].jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, useCallback } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useSelector } from "react-redux";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { useFormik } from "formik";
