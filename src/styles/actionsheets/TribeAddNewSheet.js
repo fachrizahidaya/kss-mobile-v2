59ec23ea746c3f38e926e2e05f9f75ff0cb49539
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2025-07-23 13:49:29 +0700

    fix: - api key map - google service notification

--- src/styles/actionsheets/TribeAddNewSheet.js
+++ src/styles/actionsheets/TribeAddNewSheet.js
@@ -1226,7 +1226,8 @@ const TribeAddNewSheet = (props) => {
               setErrorMessage={setErrorMessage}
               type={
                 "Clock"
-                // || "Scan QR"
+                // ||
+                // "Scan QR"
                 // ||
                 // "Generate QR"
               }
