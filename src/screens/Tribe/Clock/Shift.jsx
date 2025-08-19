bff329084c363eb628a4a77435b8fa1c0f2cdf77
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2025-08-19 17:26:31 +0700

    chore: remove unnecessary

--- src/screens/Tribe/Clock/Shift.jsx
+++ src/screens/Tribe/Clock/Shift.jsx
@@ -82,7 +82,7 @@ const Shift = () => {
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <Screen
-        screenTitle="Shift Session"
+        screenTitle="Work Session"
         returnButton={true}
         onPress={handleReturn}
         backgroundColor={Colors.secondary}
