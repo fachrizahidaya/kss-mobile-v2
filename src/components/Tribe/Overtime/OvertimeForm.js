7ab2a923b6e53af33b928e30dba73dbe95e7b9e4
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2025-08-25 16:11:58 +0700

    fix: overtime form

--- src/components/Tribe/Overtime/OvertimeForm.js
+++ src/components/Tribe/Overtime/OvertimeForm.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { Text, View } from "react-native";
 import Select from "../../../styles/forms/Select";
 import Input from "../../../styles/forms/Input";
@@ -10,12 +11,22 @@ import { Colors } from "../../../styles/Color";
 const OvertimeForm = ({ formik, optionValue, overtimes, disabled }) => {
 =======
 import { View } from "react-native";
+=======
+import { Text, View } from "react-native";
+>>>>>>> 231aa398 (fix: overtime form)
 import Select from "../../../styles/forms/Select";
 import Input from "../../../styles/forms/Input";
 import CustomTimePicker from "../../../styles/timepicker/CustomTimePicker";
+import FormButton from "../../../styles/buttons/FormButton";
+import { TextProps } from "../../../styles/CustomStylings";
+import { Colors } from "../../../styles/Color";
 
+<<<<<<< HEAD
 const OvertimeForm = ({ formik, optionValue, overtimes }) => {
 >>>>>>> 4ec5fbc8 (feat: overtime form)
+=======
+const OvertimeForm = ({ formik, optionValue, overtimes, disabled }) => {
+>>>>>>> 231aa398 (fix: overtime form)
   return (
     <View style={{ gap: 10 }}>
       <Select
@@ -28,10 +39,15 @@ const OvertimeForm = ({ formik, optionValue, overtimes }) => {
         fieldName="overtime"
       />
 <<<<<<< HEAD
+<<<<<<< HEAD
       <CustomTimePicker title="Begin Time" onChange={null} defaultValue={null} />
       <CustomTimePicker title="End Time" onChange={null} defaultValue={null} />
 =======
 >>>>>>> 4ec5fbc8 (feat: overtime form)
+=======
+      <CustomTimePicker title="Begin Time" onChange={null} defaultValue={null} />
+      <CustomTimePicker title="End Time" onChange={null} defaultValue={null} />
+>>>>>>> 231aa398 (fix: overtime form)
       <Input
         multiline
         formik={formik}
@@ -42,6 +58,9 @@ const OvertimeForm = ({ formik, optionValue, overtimes }) => {
         editable={true}
       />
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 231aa398 (fix: overtime form)
       <FormButton
         isSubmitting={formik.isSubmitting}
         disabled={disabled}
@@ -49,10 +68,13 @@ const OvertimeForm = ({ formik, optionValue, overtimes }) => {
       >
         <Text style={[TextProps, { color: Colors.fontLight }]}>Submit</Text>
       </FormButton>
+<<<<<<< HEAD
 =======
       <CustomTimePicker title="Begin Time" onChange={null} defaultValue={null} />
       <CustomTimePicker title="End Time" onChange={null} defaultValue={null} />
 >>>>>>> 4ec5fbc8 (feat: overtime form)
+=======
+>>>>>>> 231aa398 (fix: overtime form)
     </View>
   );
 };
