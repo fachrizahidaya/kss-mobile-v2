910c7dd265eea27443546a0dfad3e4f4f5c94853
Fachriza Hidaya <fachriza.hidaya98@gmail.com>
2026-03-11 10:51:13 +0700

    fix: app.js

--- App.js
+++ App.js
@@ -5,13 +5,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar, Alert, PermissionsAndroid, Platform } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { QueryClientProvider, QueryClient } from "react-query";
-import messaging, {
-  getMessaging,
-  getToken,
-  isDeviceRegisteredForRemoteMessages,
-  registerDeviceForRemoteMessages,
-  requestPermission,
-} from "@react-native-firebase/messaging";
+import messaging from "@react-native-firebase/messaging";
 import * as Notifications from "expo-notifications";
 import * as Linking from "expo-linking";
 import { RootSiblingParent } from "react-native-root-siblings";
@@ -28,59 +22,33 @@ const queryClient = new QueryClient();
 export default function App() {
   const [devicePushToken, setDevicePushToken] = useState(null);
 
-  const requestNotificationPermission = async () => {
-    const messaging = getMessaging();
-
+  const requestPermission = async () => {
     // Ask permission for ios
     if (Platform.OS === "ios") {
-      // const authStatus = await messaging().requestPermission();
-      const authStatus = await requestPermission(messaging);
-      const enabled = authStatus === 1 || authStatus === 2;
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
       if (!enabled) {
         Alert.alert(
           "You haven't given permission for Nest to send notification \n \n Please enable notifications to enhance your app experience",
         );
       }
-      const isRegistered = await isDeviceRegisteredForRemoteMessages(messaging);
-
-      if (!isRegistered) {
-        await registerDeviceForRemoteMessages(messaging);
-      }
-      // const token = await getToken(messaging);
-      // return token;
     } else {
       // Ask permission for android
       const granted = await PermissionsAndroid.check(
-<<<<<<< HEAD
         "android.permission.POST_NOTIFICATIONS",
-=======
-        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
->>>>>>> 38ec15df (fix: notification)
       );
 
       if (!granted) {
-        PermissionsAndroid.request(
-<<<<<<< HEAD
-          PermissionsAndroid.PermissionsAndroid.POST_NOTIFICATIONS,
-=======
-          PermissionsAndroid.PermissionsAndroid.POST_NOTIFICATIONS
->>>>>>> 38ec15df (fix: notification)
-        );
-        if (result !== PermissionsAndroid.RESULTS.GRANTED) {
-          console.log("Notification permission denied");
-          return null;
-        }
+        PermissionsAndroid.request("android.permission.POST_NOTIFICATIONS");
       }
-
-      // const token = await getToken(messaging);
-      // return token;
     }
   };
 
   // async function registerForPushNotificationAsync() {
-  //   const { status: existingStatus } =
-  //     await Notifications.getPermissionsAsync();
+  //   const { status: existingStatus } = await Notifications.getPermissionsAsync();
 
   //   if (existingStatus === "denied") {
   //     return null;
@@ -122,45 +90,7 @@ export default function App() {
   // }, []);
 
   useEffect(() => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    requestNotificationPermission();
-=======
-    if (requestPermission()) {
-      requestPermission();
-      messaging().getToken();
-    }
-=======
-    // if (requestPermission()) {
     requestPermission();
-    // messaging().getToken();
-    // }
->>>>>>> 287ea31f (chore: remove unnecessary)
-
-    // messaging()
-    //   .getInitialNotification()
-    //   .then(async (remoteMessage) => {});
-
-    // messaging().onNotificationOpenedApp((remoteMessage) => {});
-
-    // messaging().setBackgroundMessageHandler(async (remoteMessage) => {});
-
-    // const unsubscribe = messaging().onMessage(async (remoteMessage) => {});
-
-<<<<<<< HEAD
-    return unsubscribe;
->>>>>>> 852b1f2c (fix: firebase messaging)
-=======
-    // return unsubscribe;
->>>>>>> 287ea31f (chore: remove unnecessary)
-=======
-    requestPermission();
->>>>>>> 9972371b (chore: remove unnecessary)
-=======
-    requestNotificationPermission();
->>>>>>> 38ec15df (fix: notification)
   }, []);
 
   return (
